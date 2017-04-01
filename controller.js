b92a33af2d505c977ef5c8f02c8e9b31ec44c611
Juan Mora <raicerk@gmail.com>
2017-04-01 00:36:21 -0300

    Se crea mapreduce para la lectura de los datos

--- controller.js
+++ controller.js
@@ -14,19 +14,12 @@ exports.registro = function(req) {
       skill : req.skill
     });
 
-    var promise = data.save();
-
-    promise.then(function (doc) {
-      return true;
-    })
-    .catch(function(err){
-
-      console.log(err);
-
-      if(err.code == 11000){
-          return false;
-      }else {
-          return false;
+    data.save(function(err,res){
+      if (err) {
+        console.log("se ha producido un error al almacenar los datos");
+      }else{
+        console.log("Almacenado correctamente");
+        return true;
       }
     });
   }catch (e) {
@@ -34,3 +27,27 @@ exports.registro = function(req) {
     return false;
   }
 };
+
+exports.ConsultaDatos = function(req,res){
+  var o = {};
+  o.map = function(){
+    var datos = JSON.stringify(this.skill).replace(/[\"\]\[]/g,'');
+    var skill = datos.split(',');
+    for(i in skill){
+        emit(skill[i],1);
+    }
+  }
+
+  o.reduce = function(key,values){
+
+    var count = 0;
+    for(i in values){
+        count += values[i];
+    }
+    return count;
+  }
+
+  User.mapReduce(o, function (err, results) {
+    res.status(200).jsonp(results);
+  });
+}
--- main.js
+++ main.js
@@ -58,11 +58,16 @@ app.get('/', function(req, res){
 
 })
 
+app.get('/datos',function(req,res){
+  control.ConsultaDatos(req,res);
+});
+
 mongoose.connect('mongodb://localhost/scrapping',function(err,res){
   if (err) {
     console.log('Error de conexion a mongodb');
   }else{
     console.log('Conectado a mongodb');
+    console.log(`http://localhost:${config.puerto}`);
   }
 })
 
