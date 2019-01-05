a57da6715f5521f408983dd7b52f50068fe1ecca
Juan Mora <raicerk@gmail.com>
2019-01-05 11:36:30 -0300

    Se modifica scrapping, se elimina API y se deja solo responsabilidad de scrapping en aplicacion

--- controller.js
+++ controller.js
@@ -14,6 +14,10 @@ db.settings({
   timestampsInSnapshots: true
 });
 
+/**
+ * Lee sitio web y tabula información
+ * return boolean
+ */
 exports.scrapping = function () {
 
   console.log("Scrapping iniciado.");
@@ -56,6 +60,10 @@ exports.scrapping = function () {
   });
 };
 
+/**
+ * Almacena información en db de scrapping
+ * return void
+ */
 exports.registro = function (req) {
 
   let ms = ["ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"].indexOf(req.fecha.split(' ')[0]) + 1;
@@ -81,43 +89,10 @@ exports.registro = function (req) {
   }
 };
 
-exports.ConsultaDatos = function (req, res) {
-
-  db.collection("programacion")
-    .orderBy("fecha", "desc")
-    .get()
-    .then((querySnapshot) => {
-      let arr = [];
-      querySnapshot.forEach(function (doc) {
-        var obj = JSON.parse(JSON.stringify(doc.data()));
-        arr.push(obj);
-      });
-
-      if (arr.length > 0) {
-
-        var nuevo = [];
-
-        arr.forEach(element => {
-          for (const i in element.skill) {
-            nuevo.push(element.skill[i]);
-          }
-        });
-
-        var count = {};
-        nuevo.forEach(function (i) { count[i] = (count[i] || 0) + 1; });
-
-        res.status(200).jsonp([count]);
-
-      } else {
-        res.status(500).jsonp({ results: 'error' });
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(500).jsonp({ results: 'error' });
-    });
-};
-
+/**
+ * Metodo recursivo para autoprogramar la ejecución del scrapping de manera aleatoria con sistema de minutos maximos y minimos.
+ * return void
+ */
 exports.Programable = function () {
   var nuevaHora = moment().add(5, 'seconds').format("YYYY-MM-DD HH:mm:ss");
   console.log(`La ejecución sera el ${nuevaHora}`);
@@ -132,88 +107,4 @@ exports.Programable = function () {
       console.log(`Proxima ejecución ${nuevaHora}`);
     }
   }, 1000);
-};
-
-exports.ConsultaDatosPorFecha = function (req, res) {
-
-  meses = [
-    "2018-01",
-    "2018-02",
-    "2018-03",
-    "2018-04",
-    "2018-05",
-    "2018-06",
-    "2018-07",
-    "2018-08",
-    "2018-09",
-    "2018-10",
-    "2018-11",
-    "2018-12",
-    "2019-01",
-    "2019-02",
-    "2019-03",
-    "2019-04",
-    "2019-05",
-    "2019-06",
-    "2019-07",
-    "2019-08",
-    "2019-09",
-    "2019-10",
-    "2019-11",
-    "2019-12",
-  ]
-
-  db.collection("programacion")
-    .orderBy("fecha", "asc")
-    .get()
-    .then((querySnapshot) => {
-      let arr = [];
-      querySnapshot.forEach(function (doc) {
-        var obj = JSON.parse(JSON.stringify(doc.data()));
-        arr.push(obj);
-      });
-
-      if (arr.length > 0) {
-
-        const tercero = []
-        const datos = []
-
-        const rawSkills = []
-        // Extraccion de topicos
-        arr.forEach((entry) => rawSkills.push(...entry.skill))
-        // Limpieza de arreglo
-        const skills = [...new Set(rawSkills)]
-
-        const output = {}
-
-        skills.map((skill) => {
-          output[skill] = [] // Crea la entrada en la salida
-          const datoSkill = {}
-          arr.forEach((entry) => {
-            /** Fecha de evaluacion del ciclo */
-            const fecha = entry.fecha.split('-')[0] + '-' + entry.fecha.split('-')[1]
-            // Si la entrada en el origen de datos contiene la skill
-            if (entry.skill.findIndex((s => s === skill)) !== -1) {
-              const dato = datoSkill[fecha]
-              // Si el mes existe en datos, agrega 1, si no lo crea con valor 1
-              if (dato) {
-                datoSkill[fecha] = dato + 1
-              } else {
-                datoSkill[fecha] = 1
-              }
-            }
-          })
-          output[skill] = datoSkill
-        })
-
-        res.status(200).jsonp(output);
-
-      } else {
-        res.status(500).jsonp({ results: 'error' });
-      }
-    })
-    .catch((error) => {
-      console.log(error)
-      res.status(500).jsonp({ results: 'error' });
-    });
 };
--- server.js
+++ server.js
@@ -1,67 +1,8 @@
-//Inyección de dependencias
-let express = require('express');
-let bodyParser = require('body-parser');
-let cors = require('cors');
-const cache = require('./util/cache-middleware');
-
-//Configuración de la API
-require('events').EventEmitter.defaultMaxListeners = Infinity;
-
 //Configuración
 const config = require('./config');
 
 //Importación de controladores
 const control = require('./controller');
 
-//Inicialización de la aplicación
-let app = express();
-
-//Configuración de nuestra API
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-//Configuración de seguridad de la API
-app.use(cors());
-
-//Configuración del puerto de la API
-app.set('port', config.puerto);
-
-//Configuración de la cabecera
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', `http://${config.dominio}`);
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
-  res.setHeader('Content-Type', 'application/json');
-  next();
-});
-
-//Iniciamos las rutas de nuestro servidor/API
-let rutas = express.Router();
-
-//Ruta de bienvenida
-rutas.get('/', function(req, res) {
-  res.send({
-    'Mensaje': 'Bienvenido a la API REST de datos estadisticos informaticos'
-  });
-});
-
-//Ruta de acceso a los datos
-rutas.get('/datos', function (req, res) {
-  control.ConsultaDatos(req, res);
-});
-
-//Ruta de acceso a los datos por fecha
-rutas.get('/datosfecha', cache(30), function (req, res) {
-  control.ConsultaDatosPorFecha(req, res);
-});
-
-
-//Inicialización de las rutas
-app.use(rutas);
-
-// Inicialización del servicio
-app.listen(config.puerto, function() {
-  /// control.Programable();
-  console.log(`Node server ejecutandose en http://${config.dominio}:${config.puerto}`);
-});
+//Ejecuta el scrapping y se autoprograma
+control.Programable();
