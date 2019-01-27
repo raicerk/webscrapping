b3af1b0565fde1aae69fd1823463e84bce476ab2
Juan Mora <raicerk@gmail.com>
2019-01-27 12:02:50 -0300

    Filtros dinamicos

--- routes/graphql.js
+++ routes/graphql.js
@@ -5,12 +5,17 @@ const router = require("express").Router();
 
 const schema = buildSchema(`
   type Query {
-    skills(pais: String, order: OrderBy): [Laboral]
+    datos(where: FieldBy, order: OrderBy): [Laboral]
   }
 
   input OrderBy {
     by: String
     orientation: String
+  }
+
+  input FieldBy{
+    field: String,
+    value: String
   }
 
   type Laboral {
@@ -23,8 +28,8 @@ const schema = buildSchema(`
 `);
 
 const root = {
-  skills: async ({ pais, order }) => {
-    const snapshot = await db.collection("laboral").where("pais", "==", pais).orderBy(order.by, order.orientation).get()
+  datos: async ({ where, order }) => {
+    const snapshot = await db.collection("laboral").where(where.field, "==", where.value).orderBy(order.by, order.orientation).get()
     const { docs } = snapshot;
     const data = docs.map(doc => doc.data())
     return data;
