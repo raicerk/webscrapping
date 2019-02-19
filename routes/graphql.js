386fb29a59ba1a6125e5478894c50d55a33d695b
Juan Mora <raicerk@gmail.com>
2019-02-18 22:52:48 -0300

    Se agrega nuevo metodo que contiene toda la informacion, ademas se agrega documentacion de los campos

--- routes/graphql.js
+++ routes/graphql.js
@@ -4,8 +4,16 @@ const { buildSchema } = require('graphql');
 const router = require("express").Router();
 
 const schema = buildSchema(`
+
+  """
+  Datos estadisticos sobre ofertas laborales y conocimientos
+  requeridos para cada pais en el mundo de la tecnologia
+  """
   type Query {
-    datos(where: FieldBy, order: OrderBy): [Laboral]
+    """Datos estadisticos con filtros"""
+    estadistica(where: FieldBy, order: OrderBy): [Laboral],
+    """Datos estadisticos completos"""
+    estadisticas: [Laboral]
   }
 
   input OrderBy {
@@ -18,18 +26,33 @@ const schema = buildSchema(`
     value: String
   }
 
+  """
+  Campos disponibles de las ofertas laborales
+  y por los cuales se puede filtrar
+  """
   type Laboral {
+    """Fecha de la oferta"""
     fecha: String
+    "Pais de publicación"
     pais: String
+    "Link de acceso"
     link: String
+    "Clasificacion de la oferta"
     clasificacion: String
+    "Skill requeridos en la oferta"
     skill: [String]
   }
 `);
 
 const root = {
-  datos: async ({ where, order }) => {
+  estadistica: async ({ where, order }) => {
     const snapshot = await db.collection("laboral").where(where.field, "==", where.value).orderBy(order.by, order.orientation).get()
+    const { docs } = snapshot;
+    const data = docs.map(doc => doc.data())
+    return data;
+  },
+  estadisticas: async () => {
+    const snapshot = await db.collection("laboral").get()
     const { docs } = snapshot;
     const data = docs.map(doc => doc.data())
     return data;
