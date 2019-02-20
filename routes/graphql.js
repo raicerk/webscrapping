23a7bd1bf88bda274e13ad564e9aebc5fc998d5b
Juan Mora <raicerk@gmail.com>
2019-02-20 01:37:04 -0300

    Se agrega nuevo modelo y logica de agrupacion de datos

--- routes/graphql.js
+++ routes/graphql.js
@@ -5,15 +5,14 @@ const router = require("express").Router();
 
 const schema = buildSchema(`
 
-  """
-  Datos estadisticos sobre ofertas laborales y conocimientos
-  requeridos para cada pais en el mundo de la tecnologia
-  """
+  """Datos estadisticos sobre ofertas laborales y conocimientos requeridos para cada pais en el mundo de la tecnologia"""
   type Query {
     """Datos estadisticos con filtros"""
     estadistica(where: FieldBy, order: OrderBy): [Laboral],
     """Datos estadisticos completos"""
     estadisticas: [Laboral]
+    """Datos agrupados por fecha"""
+    agrupadoporfecha(where: FieldBy): [Agrupacion]
   }
 
   input OrderBy {
@@ -26,10 +25,15 @@ const schema = buildSchema(`
     value: String
   }
 
-  """
-  Campos disponibles de las ofertas laborales
-  y por los cuales se puede filtrar
-  """
+  """Datos de ofertas laborales por año mes y cantidad"""
+  type Datos{
+    """Año mes del skill"""
+    fecha: String
+    """Cantidad de ofertas laborales donde el skill es solicitado para la fecha que se indica"""
+    cantidad: Int
+  }
+
+  """Campos disponibles de las ofertas laborales y por los cuales se puede filtrar"""
   type Laboral {
     """Fecha de la oferta"""
     fecha: String
@@ -41,6 +45,14 @@ const schema = buildSchema(`
     clasificacion: String
     "Skill requeridos en la oferta"
     skill: [String]
+  }
+
+  """Campos disponibles para mostrar datos agrupados"""
+  type Agrupacion{
+    """Nombre del skill"""
+    skill: String
+    """Datos agrupados del skill"""
+    datos: [Datos]
   }
 `);
 
@@ -56,6 +68,52 @@ const root = {
     const { docs } = snapshot;
     const data = docs.map(doc => doc.data())
     return data;
+  },
+  agrupadoporfecha: async ({ where }) => {
+    const snapshot = await db.collection("laboral").where(where.field, "==", where.value).get()
+    const { docs } = snapshot;
+    const data = docs.map(doc => doc.data())
+
+    const rawSkills = [];
+    data.forEach((entry) => rawSkills.push(...entry.skill));
+    const skills = [...new Set(rawSkills)];
+    var iib = [];
+    const datoSkill = {};
+
+    skills.map((value) => {
+
+      var dattta = [];
+
+      data.forEach((entry) => {
+        // Fecha de evaluacion del ciclo
+        const fecha = `${entry.fecha.split('-')[0]}-${entry.fecha.split('-')[1]}`;
+        // Si la entrada en el origen de datos contiene la skill
+        if (entry.skill.findIndex(s => s === value) !== -1) {
+
+          const dato = datoSkill[fecha];
+          // Si el mes existe en datos, agrega 1, si no lo crea con valor 1
+          if (dato) {
+            datoSkill[fecha] = dato + 1;
+          } else {
+            datoSkill[fecha] = 1;
+          }
+        }
+      });
+
+      for (var i in datoSkill) {
+        dattta.push({
+          fecha: i,
+          cantidad: datoSkill[i]
+        });
+      }
+
+      iib.push({
+        skill: value,
+        datos: dattta
+      })
+    })
+
+    return iib;
   }
 };
 
